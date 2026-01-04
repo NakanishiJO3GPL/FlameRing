2897e5385a30e2421051ea35e1e62ebc229b34f5
Hirokazu Nakanishi <nakanisi.hirokazu@jp.panasonic.com>
2026-01-04 16:45:53 +0900

    Adjust animation

--- src/proximity.rs
+++ src/proximity.rs
@@ -14,11 +14,11 @@ const PROXIMITY_SENSOR_REG: u8 = 0x44;
 const PROXIMITY_SENSOR_INIT_SEQ: &[(u8, u8)] = &[
     (0x40, 0xc0), // bp7:SW reset, bp6:INT reset
     (0x40, 0x00), // bp7:SW reset, bp6:INT reset
-    (0x43, 0x01), // bp5-4:PS_GAIN, bp3-0:PERSISTENCE
+    (0x43, 0x11), // bp5-4:PS_GAIN, bp3-0:PERSISTENCE
     (0x42, 0x02), // bp5-4:ALS DATA0 gain, bp3-2:ALS DATA1 gain, bp1-0:LED current
     (0x41, 0xc6), // Measurement start
 ];
-const PROXIMITY_SENSOR_CHANGE_TH: i32 = 10;
+const PROXIMITY_SENSOR_CHANGE_TH: i32 = 100;
 
 #[embassy_executor::task]
 pub async fn proximity_sensor_task(mut i2c: I2c<'static, I2C0, i2c::Async>) {
--- src/state_machine.rs
+++ src/state_machine.rs
@@ -25,9 +25,9 @@ impl Default for State {
     }
 }
 
-const PAN_ON_TH: u16 = 500;
-const PAN_OFF_TH: u16 = 3000;
-const MAX_DUTY_MICRO: u64 = 5_000;
+const PAN_ON_TH: u16 = 1500;
+const PAN_OFF_TH: u16 = 3500;
+const MAX_DUTY_MICRO: u64 = 2_500;
 
 #[embassy_executor::task]
 pub async fn animation_state_task(
@@ -37,7 +37,7 @@ pub async fn animation_state_task(
     let mut state = State::default();
     let mut prev_state = state;
     let mut level: u8 = 5;
-    let mut time_stamp: f32 = 0.0;
+    let mut time_stamp: u16 = 0;
 
     loop {
         state = update_state(state, &mut level);
@@ -46,7 +46,7 @@ pub async fn animation_state_task(
         if state == State::Nikomi {
             // In Nikomi state, keep LED on
             nikomi_animation(time_stamp, &mut pwm0, &mut pwm1);
-            time_stamp += 0.0075;
+            time_stamp += 1;
         } else if prev_state != state {
             prev_state = state;
             match state {
@@ -101,12 +101,33 @@ fn update_state(state: State, level: &mut u8) -> State {
                     State::Standby
                 }
             }
+            Ok(Event::ProximityChanged(p)) => {
+                if p < PAN_ON_TH {
+                    State::PowerOn
+                } else {
+                    State::Standby
+                }
+            }
             _ => State::Standby,
         },
         State::PowerOn => match CHANNEL.try_receive() {
             Ok(Event::ButtonPressed(ButtonKind::Power)) => State::PowerOff,
-            Ok(Event::ButtonPressed(ButtonKind::Weak)) => State::LevelDown,
-            Ok(Event::ButtonPressed(ButtonKind::Strong)) => State::LevelUp,
+            Ok(Event::ButtonPressed(ButtonKind::Weak)) => {
+                if *level > 0 {
+                    *level -= 1;
+                    State::LevelDown
+                } else {
+                    State::PowerOn
+                }
+            }
+            Ok(Event::ButtonPressed(ButtonKind::Strong)) => {
+                if *level < 9 {
+                    *level += 1;
+                    State::LevelUp
+                } else {
+                    State::PowerOn
+                }
+            }
             Ok(Event::ButtonPressed(ButtonKind::Nikomi)) => {
                 *level = 5;
                 State::Nikomi
@@ -122,8 +143,22 @@ fn update_state(state: State, level: &mut u8) -> State {
         },
         State::PanShake => match CHANNEL.try_receive() {
             Ok(Event::ButtonPressed(ButtonKind::Power)) => State::PowerOff,
-            Ok(Event::ButtonPressed(ButtonKind::Weak)) => State::LevelDown,
-            Ok(Event::ButtonPressed(ButtonKind::Strong)) => State::LevelUp,
+            Ok(Event::ButtonPressed(ButtonKind::Weak)) => {
+                if *level > 0 {
+                    *level -= 1;
+                    State::LevelDown
+                } else {
+                    State::PowerOn
+                }
+            }
+            Ok(Event::ButtonPressed(ButtonKind::Strong)) => {
+                if *level < 9 {
+                    *level += 1;
+                    State::LevelUp
+                } else {
+                    State::PowerOn
+                }
+            }
             Ok(Event::ProximityChanged(p)) => {
                 if p > PAN_OFF_TH {
                     State::Standby
@@ -135,8 +170,22 @@ fn update_state(state: State, level: &mut u8) -> State {
         },
         State::Nikomi => match CHANNEL.try_receive() {
             Ok(Event::ButtonPressed(ButtonKind::Power)) => State::PowerOff,
-            Ok(Event::ButtonPressed(ButtonKind::Weak)) => State::LevelDown,
-            Ok(Event::ButtonPressed(ButtonKind::Strong)) => State::LevelUp,
+            Ok(Event::ButtonPressed(ButtonKind::Weak)) => {
+                if *level > 0 {
+                    *level -= 1;
+                    State::LevelDown
+                } else {
+                    State::PowerOn
+                }
+            }
+            Ok(Event::ButtonPressed(ButtonKind::Strong)) => {
+                if *level < 9 {
+                    *level += 1;
+                    State::LevelUp
+                } else {
+                    State::PowerOn
+                }
+            }
             Ok(Event::ProximityChanged(p)) => {
                 if p > PAN_OFF_TH {
                     State::Standby
@@ -146,29 +195,20 @@ fn update_state(state: State, level: &mut u8) -> State {
             }
             _ => State::Nikomi,
         },
-        State::LevelDown => {
-            if *level > 0 {
-                *level -= 1;
-            }
-            State::PowerOn
-        }
-        State::LevelUp => {
-            if *level < 9 {
-                *level += 1;
-            }
-            State::PowerOn
-        }
+        State::LevelDown => State::PowerOn,
+        State::LevelUp => State::PowerOn,
     }
 }
 
 fn nikomi_animation(
-    time_stamp: f32,
+    time_stamp: u16,
     pwm0: &mut PioPwm<'static, PIO0, 0>,
     pwm1: &mut PioPwm<'static, PIO0, 1>,
 ) {
-    let freq0: f32 = (time_stamp).sin() + 0.5;
-    let freq1: f32 = (time_stamp * 2.0).sin() + 0.5;
-    let freq2: f32 = (time_stamp * 3.8).sin() + 0.5;
+    let fts = time_stamp as f32 / 75.0;
+    let freq0: f32 = (fts).sin() + 0.5;
+    let freq1: f32 = (fts * 2.0).sin() + 0.5;
+    let freq2: f32 = (fts * 3.8).sin() + 0.5;
     let combined: f32 = freq0 * freq1 * freq2;
     let duty_percent = (combined.clamp(0.1, 1.0) * 100.0) as u8;
     let duty = core_time::Duration::from_micros(MAX_DUTY_MICRO * duty_percent as u64 / 100);
@@ -251,17 +291,14 @@ async fn level_change_animation(
     pwm1.write(core_time::Duration::from_micros(
         MAX_DUTY_MICRO * target_duty_percent as u64 / 100,
     ));
-    for duty_percent in (0..=100).skip(5) {
+    for duty_percent in (0..=100).skip(50) {
         let duty = core_time::Duration::from_micros(MAX_DUTY_MICRO * duty_percent as u64 / 100);
         pwm0.write(duty);
         em_time::Timer::after(em_time::Duration::from_millis(5)).await;
     }
-    for duty_percent in (0..=100).rev().skip(5) {
+    for duty_percent in (target_duty_percent..=100).rev().skip(50) {
         let duty = core_time::Duration::from_micros(MAX_DUTY_MICRO * duty_percent as u64 / 100);
         pwm0.write(duty);
         em_time::Timer::after(em_time::Duration::from_millis(5)).await;
     }
-    pwm0.write(core_time::Duration::from_micros(
-        MAX_DUTY_MICRO * target_duty_percent as u64 / 100,
-    ));
 }
